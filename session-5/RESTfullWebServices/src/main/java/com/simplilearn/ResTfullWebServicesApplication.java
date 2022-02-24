dedd0f3eec7903a217829abf4a28ee0453a57508
kurma narsimlu <kurma narsimlu>
2022-02-24 23:05:01 +0530

    session-5

---
+++ session-5/RESTfullWebServices/src/main/java/com/simplilearn/ResTfullWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTfullWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResTfullWebServicesApplication.class, args);
+	}
+
+}
